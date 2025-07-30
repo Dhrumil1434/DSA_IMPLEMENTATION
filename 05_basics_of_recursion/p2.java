c64c183cee1ff7772b99c2ddb7b7abfacd9f1ae5
Dhrumil1434 <dhrumilpanchal1434@gmail.com>
2025-07-30 14:23:44 +0530

    feat: added time complexity parameters

--- 05_basics_of_recursion/p2.java
+++ 05_basics_of_recursion/p2.java
@@ -1,16 +1,20 @@
 /*
- * following is the code to print the string by n times 
+ * following is the code to print the string by n times using recursion
 */
 public class p2 {
-    public static void demo(int i,int j)
-    {
+    public static void demo(int i, int j) {
         if (i > j) {
             return;
         }
         System.out.println("Hello");
         demo(i + 1, j);
     }
+
     public static void main(String[] args) {
         demo(1, 5);
     }
 }
+/*
+ * time complexity is O(n)
+ * space complexity is O(n)
+*/
